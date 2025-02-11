4f862ac3184bbe8ee4c051c828f7c5d1290a4ea7
user05 <user05@mail.ru>
2025-02-11 15:39:28 +0300

    WPF_Session2

---
+++ WpfApp1/WpfApp1/Models/DepartmentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public class DepartmentModel
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; } = null!;
+
+        public string? Description { get; set; }
+        public int? ManagerId { get; set; }
+    }
+}
---
+++ WpfApp1/WpfApp1/Models/PositionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public class PositionModel
+    {
+        public int PositionId { get; set; }
+        public string PositionName { get; set; } = null!;
+    }
+}
